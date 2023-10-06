96d90cb23586a2c603bf4c9d2fdfe2d546e05f27
Ethan Ermovick <keg9ve@virginia.edu>
2023-10-06 01:34:05 -0400

    add compression

--- club_compass/settings.py
+++ club_compass/settings.py
@@ -134,6 +134,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #django_heroku.settings(locals())
