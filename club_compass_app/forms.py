3e3e13114aef37c0f29aaacfb8875a0d52ea3baa
Ethan Ermovick <keg9ve@virginia.edu>
2023-10-25 00:21:32 -0400

    message form working

--- club_compass_app/forms.py
+++ club_compass_app/forms.py
@@ -1,41 +1,47 @@
 from django import forms
 
+
+class MessageForm(forms.Form):
+    message_text = forms.CharField(label="Enter Message", max_length=1000,
+                                   widget=forms.Textarea(attrs={"class": "form-control",
+                                                                "placeholder": "Enter message to send to all club "
+                                                                               "members"}))
+
+
 class ClubForm(forms.Form):
-    club_name = forms.CharField(label="Enter your club's name", max_length=50, 
-                                    widget = forms.TextInput(attrs={"placeholder": "Enter your club name here",
-                                                                    "class": "form-control"}))
-    
+    club_name = forms.CharField(label="Enter your club's name", max_length=50,
+                                widget=forms.TextInput(attrs={"placeholder": "Enter your club name here",
+                                                              "class": "form-control"}))
+
     description = forms.CharField(label='Enter a little description about your club here', max_length=2000,
-                               widget = forms.Textarea(attrs={"class": "form-control",
-                                                                "placeholder": "Enter a description about your club"}))
-    
+                                  widget=forms.Textarea(attrs={"class": "form-control",
+                                                               "placeholder": "Enter a description about your club"}))
+
     public = forms.BooleanField(label="Make your club public?",
-                                widget = forms.CheckboxInput(attrs={"class": "form-check-input"}), required=False)
+                                widget=forms.CheckboxInput(attrs={"class": "form-check-input"}), required=False)
 
 
 class EventForm(forms.Form):
     event_name = forms.CharField(label="Enter the title of your event", max_length=50,
                                  widget=forms.TextInput(attrs={"placeholder": "Enter your event name here",
-                                                              "class": "form-control"}))
-    
+                                                               "class": "form-control"}))
+
     description = forms.CharField(label="Enter a description about your event", max_length=2000,
-                                    widget=forms.Textarea(attrs={"class": "form-control",
-                                                                 "placeholder": "Enter a description about your event"}))
-    
-    date = forms.DateField(label="Enter the date of your event", 
+                                  widget=forms.Textarea(attrs={"class": "form-control",
+                                                               "placeholder": "Enter a description about your event"}))
+
+    date = forms.DateField(label="Enter the date of your event",
                            widget=forms.DateInput(attrs={"class": "form-control",
-                                                        "placeholder": "Enter the date of your event"}))
-    
-    start_time = forms.TimeField(label="Enter the start time of your event", 
+                                                         "placeholder": "Enter the date of your event"}))
+
+    start_time = forms.TimeField(label="Enter the start time of your event",
                                  widget=forms.TimeInput(attrs={"class": "form-control",
-                                                            "placeholder": "Enter the start time of your event"}))
-    
-    end_time = forms.TimeField(label="Enter the end time of your event", 
+                                                               "placeholder": "Enter the start time of your event"}))
+
+    end_time = forms.TimeField(label="Enter the end time of your event",
                                widget=forms.TimeInput(attrs={"class": "form-control",
-                                                            "placeholder": "Enter the end time of your event"}))
-    
+                                                             "placeholder": "Enter the end time of your event"}))
+
     location = forms.CharField(label="Enter the location of your event", max_length=100,
-                                 widget=forms.TextInput(attrs={"class": "form-control",
-                                                                 "placeholder": "Enter the location of your event"}))
-    
-    
+                               widget=forms.TextInput(attrs={"class": "form-control",
+                                                             "placeholder": "Enter the location of your event"}))
--- club_compass_app/views.py
+++ club_compass_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.views import generic
 from .models import Club, Membership
-from .forms import ClubForm
+from .forms import ClubForm, MessageForm
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import UserPassesTestMixin
@@ -17,6 +17,43 @@ def login(request):
         # If the user is authenticated, redirect them to the home page
         return redirect('/home/')
     return render(request, 'club_compass_app/accountTypeSelectionScreen.html')
+
+
+class SendMessage(UserPassesTestMixin, generic.FormView):
+    template_name = "club_compass_app/send_message.html"
+    form_class = MessageForm
+    success_url = "/"
+
+    def form_valid(self, form):
+        if self.request.user.is_authenticated \
+                and Club.check_user_owns_club(self.request.user):
+            message = form.cleaned_data['message_text']
+            club = Club.get_club_by_owner(self.request.user)
+            print(f"send {message} to {club.get_name()}")
+            # club_name = form.cleaned_data['club_name']
+            # description = form.cleaned_data['description']
+            # owner = self.request.user
+            # public = form.cleaned_data['public']
+            # club = Club(name=club_name, description=description, owner=owner, public=public)
+            # club.save()
+            return super().form_valid(form)
+        else:
+            return redirect("/")
+
+    def handle_no_permission(self) -> HttpResponseRedirect:
+        return redirect("/")
+
+    def test_func(self):
+        if not self.request.user.is_authenticated:
+            return False
+
+        if Membership.is_user_account(self.request.user):
+            return False
+
+        if not Club.check_user_owns_club(self.request.user):
+            return False
+
+        return True
 
 
 class Login(UserPassesTestMixin, generic.FormView):
