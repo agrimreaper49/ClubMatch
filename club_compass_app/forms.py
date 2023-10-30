331bff38fff15d19e7f115eb9f0da503e8fcf977
Ethan Ermovick <keg9ve@virginia.edu>
2023-10-29 23:25:39 -0400

    seperatoe out date form functionality

--- club_compass_app/forms.py
+++ club_compass_app/forms.py
@@ -1,18 +1,6 @@
 from django import forms
 
 
-class When2MeetForm(forms.Form):
-    event_name = forms.CharField(label="Enter the title of your event", max_length=50,
-                                 widget=forms.TextInput(attrs={"placeholder": "Enter your event name here",
-                                                               "class": "form-control"}))
-
-    # dates = forms.DateField(label="Meeting Date(s)", widget=forms.DateInput())
-
-    dates = forms.DateField(label="Enter the date of your event",
-                           widget=forms.DateInput(attrs={"class": "form-control",
-                                                         "placeholder": "Enter the date of your event"}))
-
-
 class MessageForm(forms.Form):
     message_text = forms.CharField(label="Enter Message", max_length=1000,
                                    widget=forms.Textarea(attrs={"class": "form-control",
@@ -33,11 +21,45 @@ class ClubForm(forms.Form):
                                 widget=forms.CheckboxInput(attrs={"class": "form-check-input"}), required=False)
 
 
-class EventForm(forms.Form):
+class TimeForm(forms.Form):
     HOURS = [(f'{i:02d}', f'{i:02d}') for i in range(1, 13)]
     MINUTES = [(f'{i:02d}', f'{i:02d}') for i in range(0, 60)]
     DAY_NIGHT = [("AM", "AM"), ("PM", "PM")]
 
+    ## TIMES ##
+
+    ## START TIME ##
+    start_hour = forms.ChoiceField(label="Enter the start time of your event", choices=HOURS,
+                                   widget=forms.Select(attrs={"class": "form-control"}, choices=HOURS))
+
+    start_minute = forms.ChoiceField(label="Enter the start time of your event", choices=MINUTES,
+                                     widget=forms.Select(attrs={"class": "form-control"}, choices=MINUTES))
+
+    start_day_night = forms.ChoiceField(label="AM or PM?", choices=DAY_NIGHT,
+                                        widget=forms.Select(attrs={"class": "form-control"}, choices=DAY_NIGHT))
+
+    ## END TIME ##
+    end_hour = forms.ChoiceField(label="Enter the end time of your event", choices=HOURS,
+                                 widget=forms.Select(attrs={"class": "form-control"}, choices=HOURS))
+
+    end_minute = forms.ChoiceField(label="Enter the start hour of your event", choices=MINUTES,
+                                   widget=forms.Select(attrs={"class": "form-control"}, choices=MINUTES))
+
+    end_day_night = forms.ChoiceField(label="AM or PM?", choices=DAY_NIGHT,
+                                      widget=forms.Select(attrs={"class": "form-control"}, choices=DAY_NIGHT))
+
+
+class When2MeetForm(TimeForm):
+    event_name = forms.CharField(label="Enter the title of your event", max_length=50,
+                                 widget=forms.TextInput(attrs={"placeholder": "Enter your event name here",
+                                                               "class": "form-control"}))
+
+    date = forms.DateField(label="Enter the date of your event",
+                           widget=forms.DateInput(attrs={"class": "form-control",
+                                                         "placeholder": "YYYY-MM-DD"}))
+
+
+class EventForm(TimeForm):
     event_name = forms.CharField(label="Enter the title of your event", max_length=50,
                                  widget=forms.TextInput(attrs={"placeholder": "Enter your event name here",
                                                                "class": "form-control"}))
@@ -46,32 +68,10 @@ class EventForm(forms.Form):
                                   widget=forms.Textarea(attrs={"class": "form-control",
                                                                "placeholder": "Enter a description about your event"}))
 
-    date = forms.DateField(label="Enter the date of your event", 
+    date = forms.DateField(label="Enter the date of your event",
                            widget=forms.DateInput(attrs={"class": "form-control",
                                                          "placeholder": "YYYY-MM-DD"}))
-    
+
     location = forms.CharField(label="Enter the location of your event", max_length=100,
                                widget=forms.TextInput(attrs={"class": "form-control",
                                                              "placeholder": "Enter the location of your event"}))
-    
-    ## TIMES ##
-
-    ## START TIME ##
-    start_hour = forms.ChoiceField(label="Enter the start time of your event", choices=HOURS,
-                                   widget=forms.Select(attrs={"class": "form-control"}, choices=HOURS))
-    
-    start_minute = forms.ChoiceField(label="Enter the start time of your event", choices=MINUTES,
-                                   widget=forms.Select(attrs={"class": "form-control"}, choices=MINUTES))
-    
-    start_day_night = forms.ChoiceField(label="AM or PM?", choices=DAY_NIGHT,
-                                    widget=forms.Select(attrs={"class": "form-control"}, choices=DAY_NIGHT))
-    
-    ## END TIME ##
-    end_hour = forms.ChoiceField(label="Enter the end time of your event", choices=HOURS,
-                                   widget=forms.Select(attrs={"class": "form-control"}, choices=HOURS))
-    
-    end_minute = forms.ChoiceField(label="Enter the start hour of your event", choices=MINUTES,
-                                   widget=forms.Select(attrs={"class": "form-control"}, choices=MINUTES))
-    
-    end_day_night = forms.ChoiceField(label="AM or PM?", choices=DAY_NIGHT,
-                                    widget=forms.Select(attrs={"class": "form-control"}, choices=DAY_NIGHT))
